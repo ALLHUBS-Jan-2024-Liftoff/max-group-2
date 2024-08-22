cc6e315401bf8793897892cf2ab6d74c51c94ef5
UGcode101 <142420124+UGcode101@users.noreply.github.com>
2024-08-22 13:05:43 -0500

    this should be able to accept lists now

--- threemix-backend/src/main/java/org/launchcode/threemix/controller/BlockedContentController.java
+++ threemix-backend/src/main/java/org/launchcode/threemix/controller/BlockedContentController.java
@@ -18,21 +18,23 @@ public class BlockedContentController {
     @Autowired
     private UserService userService;
 
-    // POST and GET for blocked artists
+    // Blocking a list of artists
     @PostMapping("/blockedArtist")
-    public void blockArtist(@RequestParam String artistId,
-                            HttpSession session) {
+    public void blockArtists(@RequestParam List<String> artistIds, HttpSession session) {
         String spotifyId = userService.getUserId(session);
         User user = userService.findUserBySpotifyId(spotifyId);
-        if (user.getBlockedArtists().stream().map(BlockedArtist::getArtistId).noneMatch(id -> id.equals(artistId))) {
-            BlockedArtist blockedArtist = new BlockedArtist();
-            blockedArtist.setArtistId(artistId);
-            blockedArtist.setUser(user);
-            user.getBlockedArtists().add(blockedArtist);
-            userService.saveUser(user);
+        for (String artistId : artistIds) {
+            if (user.getBlockedArtists().stream().map(BlockedArtist::getArtistId).noneMatch(id -> id.equals(artistId))) {
+                BlockedArtist blockedArtist = new BlockedArtist();
+                blockedArtist.setArtistId(artistId);
+                blockedArtist.setUser(user);
+                user.getBlockedArtists().add(blockedArtist);
+            }
         }
+        userService.saveUser(user);  // Save user only once after all artists are added
     }
 
+    // Retrieving blocked artists
     @GetMapping("/blockedArtist")
     public List<BlockedArtist> getBlockedArtists(HttpSession session) {
         String spotifyId = (String) session.getAttribute("spotifyId");
@@ -40,27 +42,31 @@ public class BlockedContentController {
         return userService.findBlockedArtistByUser(user);
     }
 
-    // Deleting a specific blocked artist
-    @DeleteMapping("/blockedArtist/{id}")
-    public void unblockArtist(@PathVariable Long id) {
-        userService.deleteBlockedArtistById(id);
-    }
-
-    // POST and GET for blocked songs
-    @PostMapping("/blockedSong")
-    public void blockSong(@RequestParam String songId,
-                          HttpSession session) {
-        String spotifyId = userService.getUserId(session);
-        User user = userService.findUserBySpotifyId(spotifyId);
-        if (user.getBlockedSongs().stream().map(BlockedSong::getSongId).noneMatch(id -> id.equals(songId))) {
-            BlockedSong blockedSong = new BlockedSong();
-            blockedSong.setSongId(songId);
-            blockedSong.setUser(user);
-            user.getBlockedSongs().add(blockedSong);
-            userService.saveUser(user);
+    // Unblocking a list of artists
+    @DeleteMapping("/blockedArtist")
+    public void unblockArtists(@RequestParam List<Long> ids) {
+        for (Long id : ids) {
+            userService.deleteBlockedArtistById(id);
         }
     }
 
+    // Blocking a list of songs
+    @PostMapping("/blockedSong")
+    public void blockSongs(@RequestParam List<String> songIds, HttpSession session) {
+        String spotifyId = userService.getUserId(session);
+        User user = userService.findUserBySpotifyId(spotifyId);
+        for (String songId : songIds) {
+            if (user.getBlockedSongs().stream().map(BlockedSong::getSongId).noneMatch(id -> id.equals(songId))) {
+                BlockedSong blockedSong = new BlockedSong();
+                blockedSong.setSongId(songId);
+                blockedSong.setUser(user);
+                user.getBlockedSongs().add(blockedSong);
+            }
+        }
+        userService.saveUser(user);  // Save user only once after all songs are added
+    }
+
+    // Retrieving blocked songs
     @GetMapping("/blockedSong")
     public List<BlockedSong> getBlockedSongs(HttpSession session) {
         String spotifyId = (String) session.getAttribute("spotifyId");
@@ -68,9 +74,11 @@ public class BlockedContentController {
         return userService.findBlockedSongsByUser(user);
     }
 
-    // Deleting a specific blocked song
-    @DeleteMapping("/blockedSong/{id}")
-    public void unblockSong(@PathVariable Long id) {
-        userService.deleteBlockedSongById(id);
+    // Unblocking a list of songs
+    @DeleteMapping("/blockedSong")
+    public void unblockSongs(@RequestParam List<Long> ids) {
+        for (Long id : ids) {
+            userService.deleteBlockedSongById(id);
+        }
     }
 }
